2aabff18c8b871a8ed5816d90e1e3311b8c451d5
zzz <1796426212@qq.com>
2025-09-18 16:37:58 +1000

    master commit2

--- src/main/java/com/example/tliaswebmanagement/controller/test.java
+++ src/main/java/com/example/tliaswebmanagement/controller/test.java
@@ -3,5 +3,6 @@ package com.example.tliaswebmanagement.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello hot-fix");
+        System.out.println("master test");
     }
 }
