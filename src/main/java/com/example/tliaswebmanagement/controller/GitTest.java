87c4536ccdac6341f6749dba5b56514ca6ac8f0a
zyw888 <zhouyawen030814@gmail.com>
2025-09-18 17:25:23 +1000

    pull commit

--- src/main/java/com/example/tliaswebmanagement/controller/GitTest.java
+++ src/main/java/com/example/tliaswebmanagement/controller/GitTest.java
@@ -3,5 +3,6 @@ package com.example.tliaswebmanagement.controller;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("push test!!!");
+        System.out.println("pull test!!!");
     }
 }
